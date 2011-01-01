6f8dba3ae6f96a0ea88615a28502042c176646a2
Your Name <tomyhero@cpan.org>
2011-01-01 18:42:14 +0900

    initial commit

---
+++ lib/Twh.pm
@@ -0,0 +1,33 @@
+package Twh;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Twh -
+
+=head1 SYNOPSIS
+
+  use Twh;
+
+=head1 DESCRIPTION
+
+Twh is
+
+=head1 AUTHOR
+
+polocky E<lt>polocky+cpan@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
